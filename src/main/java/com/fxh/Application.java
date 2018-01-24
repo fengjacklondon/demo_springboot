2c173a64af5c9c1fdb735e13b987723a88b5bcee
fengjacklondon <1634571635@qq.com>
2018-01-24 20:16:46 +0800

    spring boot

---
+++ src/main/java/com/fxh/Application.java
@@ -0,0 +1,33 @@
+package com.fxh;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class Application  extends SpringBootServletInitializer implements WebApplicationInitializer{
+
+    @Bean
+    public PlatformTransactionManager txManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String [] args) throws  Exception{
+        SpringApplication.run(Application.class,args);
+
+
+    }
+}
